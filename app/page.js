44bb9b28757cefe1b01c8180079f381440ee689e
Adu Bright <nargatinc@gmail.com>
2025-10-04 12:13:21 +0000

    Fetch and display post content and images

--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
+
+
 // app/page.js
 import { gql } from "@apollo/client";
 import { client } from "../lib/apollo";
+import Image from "next/image";
 
 async function getPosts() {
   const GET_POSTS = gql`
@@ -9,6 +12,12 @@ async function getPosts() {
         nodes {
           slug
           title
+          content // Add this line to fetch the post content
+          featuredImage {
+            node {
+              sourceUrl
+            }
+          }
         }
       }
     }
@@ -16,6 +25,13 @@ async function getPosts() {
   const { data } = await client.query({ query: GET_POSTS });
   return data.posts.nodes;
 }
+// ... rest of the code is unchanged
+
+
+
+
+// app/page.js
+// ... (imports and getPosts function) ...
 
 export default async function Home() {
   const posts = await getPosts();
@@ -25,7 +41,19 @@ export default async function Home() {
       <h1>My Headless Blog</h1>
       <ul>
         {posts.map((post) => (
-          <li key={post.slug}>{post.title}</li>
+          <li key={post.slug}>
+            <h2>{post.title}</h2>
+            {post.featuredImage?.node?.sourceUrl && (
+              <Image
+                src={post.featuredImage.node.sourceUrl}
+                alt={post.title}
+                width={500}
+                height={300}
+              />
+            )}
+            {/* Add this section to render the post content */}
+            <div dangerouslySetInnerHTML={{ __html: post.content }} />
+          </li>
         ))}
       </ul>
     </main>

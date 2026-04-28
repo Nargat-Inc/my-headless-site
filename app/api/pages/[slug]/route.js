2a7cc044fb0bc1b90453d5012d6df63f8517e34d
Nargat <99654879+Nargat-Inc@users.noreply.github.com>
2026-04-28 10:16:38 +0000

    Implement GET route for page data retrieval

---
+++ app/api/pages/[slug]/route.js
@@ -0,0 +1,23 @@
+import { gql } from "@apollo/client";
+import { client } from "../../../../lib/apollo";
+
+export async function GET(req, { params }) {
+  const token = req.headers.get("x-api-secret");
+  if (token !== process.env.API_SECRET) {
+    return Response.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const { data } = await client.query({
+    query: gql`
+      query GetPage($slug: String!) {
+        pageBy(uri: $slug) {
+          title
+          content
+        }
+      }
+    `,
+    variables: { slug: params.slug },
+  });
+
+  return Response.json(data.pageBy);
+}
